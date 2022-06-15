200720b5994cac45e517a93b79a0089ea0e4f795
hayato2022 <gaomusunxiang@gmail.com>
2022-06-15 07:43:00 +0000

    [add]アイコン修正、バリデーション設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new,:index,:show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 
-  get "homes/about", as:'about'
+  get 'home/about'=>'homes#about', as:'about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
